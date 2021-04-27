eb64e02c8e3d3f99e6595b4c0283ed7b292aebf3
Marcos Villanueva <marcossantiago.villanueva@mercadolibre.com>
2021-04-27 01:31:45 -0300

    Add UDP Client

---
+++ client/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+        "bufio"
+        "fmt"
+        "net"
+        "os"
+)
+
+const (
+	HOST = "127.0.0.1"	// Local Host
+	PORT = ":8080"		// Nombre del puerto
+	PROTOCOL = "udp4"		// Nombre del protocolo
+)
+
+func main() {
+
+	s, err := net.ResolveUDPAddr(PROTOCOL, HOST+PORT)
+	if err != nil {
+		fmt.Println("Hubo un error creando el UDP Address:", err)
+		return
+	}
+
+	c, err := net.DialUDP(PROTOCOL, nil, s)
+	if err != nil {
+		fmt.Println("Hubo un error creando el Dial UDP:", err)
+		return
+	}
+
+	fmt.Printf("El servidor UDP es %s\n", c.RemoteAddr().String())	// Solo para tener de Info
+	fmt.Println("Tu Address local es", c.LocalAddr().String())		// Solo para tener de Info
+	defer c.Close()
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Escribi tu mensaje: ")
+		text, _ := reader.ReadString('\n')
+		data := []byte(text)
+		_, err = c.Write(data)
+		if err != nil {
+			fmt.Println("Hubo un error en el mensaje:", err)
+			return
+		}
+
+		buffer := make([]byte, 1024)
+		n, _, err := c.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println("Hubo un error leyendo la respuesta del servidor:", err)
+			return
+		}
+		fmt.Println(string(buffer[0:n]))
+	}
+}
+      
